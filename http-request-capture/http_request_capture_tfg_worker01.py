cf3ccc62fb539af8ab4caa9a25a8f8338c89a0ca
Alberto Crego <sp81982@oc0388645808.ibm.com>
2018-09-04 23:14:29 +0200

    test

--- http-request-capture/http_request_capture_tfg_worker01.py
+++ http-request-capture/http_request_capture_tfg_worker01.py
@@ -30,8 +30,8 @@ def printit():
     if (COUNTGET.count('192.168.31.102:30080/get-ip') > 5):
         del COUNTGET[:]
         print "bbb"
-        #subprocess.call("sshpass -p 'raspberry' ssh -T pi@192.168.31.232 './tfg_project/nodeSelector/move2worker01.sh'", shell=True)
-        subprocess.call("ssh pi@192.168.31.232 './tfg_project/nodeSelector/move2worker01.sh'", shell=True)
+        subprocess.call("sshpass -p 'raspberry' ssh -T pi@192.168.31.232 './tfg_project/nodeSelector/move2worker01.sh'", shell=True)
+        #subprocess.call("ssh pi@192.168.31.232 './tfg_project/nodeSelector/move2worker01.sh'", shell=True)
     else:
         del COUNTGET[:]
 
