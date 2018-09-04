7e0db382879531051e051544470842fc3d1b3030
Alberto Crego <sp81982@oc0388645808.ibm.com>
2018-09-04 19:07:00 +0200

    test

--- http-request-capture/http_request_capture_tfg.py
+++ http-request-capture/http_request_capture_tfg.py
@@ -29,7 +29,7 @@ def printit():
         #os.system("ssh -T pi@192.168.31.232 './tfg_project/nodeSelector/move2worker00.sh'")
         #subprocess.call(["sshpass", "-p", "raspberry", "ssh", "pi@192.168.31.232", "bash", "-s" , "<", "/home/worker00/Documents/tfg_project/nodeSelector/move2worker00.sh"])
         #subprocess.call(["ssh", "pi@192.168.31.232", "'./tfg_project/nodeSelector/move2worker00.sh'"])
-        subprocess.call("ssh -T pi@192.168.31.232 './tfg_project/nodeSelector/move2worker00.sh'", shell=True)
+        subprocess.call("sshpass -p 'raspberry' ssh -T pi@192.168.31.232 './tfg_project/nodeSelector/move2worker00.sh'", shell=True)
         #os.system("sshpass -p \'raspberry\'ssh pi@192.168.31.232  \'./home/pi/tfg_project/nodeSelector/move2worker00.sh\'")
         del COUNTGET[:]
         #proc = subprocess.Popen('ssh pi@192.168.31.232 \'./tfg_project/nodeSelector/move2worker00.sh\'',shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,)
